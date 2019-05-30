6d91d7cecfd89d62c89da6a32fb53c3fd09a1827
rahat14 <rahatshovo6666@gmail.com>
2019-05-31 00:41:32 +0600

    0.0.1
    added time & Date Picker

--- app/src/main/java/com/metacoders/hurry/MainActivity.java
+++ app/src/main/java/com/metacoders/hurry/MainActivity.java
@@ -1,13 +1,91 @@
 package com.metacoders.hurry;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+
+           TextView  datePicker  , timePicker ;
+    DatePickerDialog  datePickerDialog ;
+    String amPmChecker ;
+
+
+    Calendar c ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timePicker = findViewById(R.id.timeEdit);
+        datePicker = findViewById(R.id.dateEdit);
+
+
+        datePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // calling the date Picker
+
+                Calendar c = Calendar.getInstance();
+
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                int month = c.get(Calendar.MONTH);
+                int year = c.get(Calendar.YEAR);
+
+                datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                        datePicker.setText(dayOfMonth + "/"+(month+1)+"/"+year);
+
+                    }
+                } ,year , month , day);
+
+                    datePickerDialog.show();
+            }
+        });
+
+
+
+        timePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //calling the time picker
+
+                TimePickerDialog timePickerDialog  = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                        if (hourOfDay >= 12){
+                            amPmChecker = "AM";
+
+                        }
+                        else  {
+                            amPmChecker= "PM";
+                        }
+
+
+                        timePicker.setText(hourOfDay+":"+minute+" "+amPmChecker);
+
+                    }
+                }, 0,0, false);
+
+
+                timePickerDialog.show();
+            }
+        });
+
+
     }
 }
