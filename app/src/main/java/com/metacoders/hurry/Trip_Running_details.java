9cba1863db4a354a26113812f2333279166e3125
shaheensms <shaheensms111@gmail.com>
2019-07-08 22:48:46 +0600

    Trip Details page designed

---
+++ app/src/main/java/com/metacoders/hurry/Trip_Running_details.java
@@ -0,0 +1,14 @@
+package com.metacoders.hurry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Trip_Running_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip__running_details);
+    }
+}
