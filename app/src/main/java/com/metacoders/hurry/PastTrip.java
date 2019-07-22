9d1d7eb79a9692c2eae0e564c6c204e824ccf6d2
shaheensms <shaheensms111@gmail.com>
2019-07-22 23:45:37 +0600

    on going Trip Details page designed and others

---
+++ app/src/main/java/com/metacoders/hurry/PastTrip.java
@@ -0,0 +1,14 @@
+package com.metacoders.hurry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PastTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_trip);
+    }
+}
